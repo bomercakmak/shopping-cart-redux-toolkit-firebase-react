7638e02c4194baab986198d29d968d5e53c3571f
bomercakmak <bahadir0646@gmail.com>
2021-05-14 16:40:47 +0200

    I Created firebase project and storaged cart data in there. (close #5)

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
@@ -5,6 +6,17 @@ import { useSelector } from "react-redux";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  useEffect(() => {
+    fetch(
+      "https://cart-redux-react-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+      {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      }
+    );
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
