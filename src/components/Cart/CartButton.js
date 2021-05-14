e33e5c812bef8dec22f07f2ec24fa2cd0b24d05b
bomercakmak <bahadir0646@gmail.com>
2021-05-14 11:06:10 +0200

    I added dynamic cart button badge for totalQuantity.

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,17 +1,17 @@
 import classes from "./CartButton.module.css";
 import { uiActions } from "../../store/ui-slice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const CartButton = (props) => {
   const dispatch = useDispatch();
-
+  const cartQuantity = useSelector((state) => state.cart.totalQuantity);
   const cartButtonHandler = () => {
     dispatch(uiActions.toggle());
   };
   return (
     <button onClick={cartButtonHandler} className={classes.button}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -10,6 +10,7 @@ const cartSlice = createSlice({
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
           itemId: newItem.id,
@@ -26,12 +27,17 @@ const cartSlice = createSlice({
     removeItemFromCart(state,action) {
         const id = action.payload;
         const existingItem = state.items.find(item => item.id === id);
-
+        state.totalQuantity--;
         if(existingItem.quantity === 1){
             state.items = state.items.filter((item) => item.id !== id);
         } else {
             existingItem.quantity--;
+            existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
         }
     },
   },
 });
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
